d1fc405f709d7a5a4eaa9e9436b6778dd838d535
Debodip Chowdhury <75762212+Debodip-cloud@users.noreply.github.com>
2024-08-13 16:19:41 +0100

    Update run_all_commands.bat

--- run_all_commands.bat
+++ run_all_commands.bat
@@ -1,11 +1,3 @@
-dotnet run 14 0 0 6 0 0 0
-dotnet run 13 0 0 7 0 0 0
-dotnet run 12 0 0 8 0 0 0
-dotnet run 11 0 0 9 0 0 0
-dotnet run 10 0 0 10 0 0 0
-dotnet run 9 0 0 11 0 0 0
-dotnet run 8 0 0 12 0 0 0
-dotnet run 7 0 0 13 0 0 0
 dotnet run 6 0 0 14 0 0 0
 dotnet run 5 0 0 15 0 0 0
 dotnet run 4 0 0 16 0 0 0

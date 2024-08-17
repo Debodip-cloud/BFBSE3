c92cd120268951e7dcacb187be17d201ab698e70
Debodip Chowdhury <vn23094@bristol.ac.uk@csharp.wuqcmg5zxo1uneaep2k0rb14be.zx.internal.cloudapp.net>
2024-08-17 02:29:32 +0000

    new files

--- obj/Debug/net8.0/BFBSE.AssemblyInfo.cs
+++ obj/Debug/net8.0/BFBSE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BFBSE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6d169535ce6f33b49c38340203b4ff61b3691b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acfee9cd10f7997bf02c40e4ea4bff2b4eb874f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BFBSE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BFBSE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
